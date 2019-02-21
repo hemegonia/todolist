b9f68262984e659a4ea032e1db501477c7283b3e
Eric Tran <erictran88@gmail.com>
2019-02-21 15:50:19 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+
+Summary
+    Create a todo list web application using node and mongo. Implement user auth and store todolists per user. Use CRUD for todo items.
+
+Planning
+    Database
+        Users
+        Todo item
+        Users <- Lists <- Todo
+        
+    Interface
+        li
+        checkboxes
+        strikethrough
+        css framework
+        flash messages
+
+
+Future Enhancement Ideas
+    List sharing between users
+    Todo Due Dates
+    Email or Text alerts
+    List change subscription
+    Weekly summary per user
+    Multiple lists per user
+    Tutorial
---
+++ app.js
@@ -0,0 +1,46 @@
+var express = require('express'),
+	app = express(),
+	mongoose = require('mongoose'),
+	bodyParser = require('body-parser');
+
+app.set('view engine', 'ejs');
+
+/////////////////////////////////////////////
+//      ROUTES
+
+//LANDING
+app.get('/', function(req, res) {
+	res.send('HOMEPAGE');
+});
+
+//LOGIN
+//SIGN UP
+
+/////////////////////////////////////////
+//      todo CRUD ROUTES
+//INDEX
+//SHOW
+//EDIT
+//UPDATE
+//NEW
+//CREATE
+//DELETE
+
+/////////////////////////////////////////
+//      list CRUD ROUTES
+//INDEX
+//SHOW
+//EDIT
+//UPDATE
+//NEW
+//CREATE
+//DELETE
+
+app.listen(process.env.PORT || 3000, function(err) {
+	if (err) {
+		console.log(err);
+	}
+	else {
+		console.log('Server is up');
+	}
+});
